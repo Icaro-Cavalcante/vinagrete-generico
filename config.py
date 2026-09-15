f50c5e4ae3dea90dac39f1afb2796ca800b078c7
Gabriel Souza <santos.gabriel@aluno.ufca.edu.br>
2026-09-15 11:09:22 -0300

    refactor: update ROI settings

--- config.py
+++ config.py
@@ -8,13 +8,12 @@ DEFECT_CLASSES = set(os.getenv("DEFECT_CLASSES", {"damage"}))
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/data/vinagrete.db")
 
-# Motion/ROI settings
-# Example values, can be overridden by env variables if desired
-ROI_X_BOUNDS = (100, 540)
-MIN_CONTOUR_AREA = int(os.getenv("MIN_CONTOUR_AREA", 500))
-BG_SUBTRACTOR_THRESHOLD = int(os.getenv("BG_SUBTRACTOR_THRESHOLD", 16))
-
 # Câmera
 CAMERA_INDEX = 0  
 CAMERA_RESOLUTION = (1296, 972)
 CAMERA_FRAMERATE = 60
+
+# MotionROI settings
+ROI_DIMENSIONS = (400, 400) # (width, height)
+MIN_CONTOUR_AREA = int(os.getenv("MIN_CONTOUR_AREA", 500))
+BG_SUBTRACTOR_THRESHOLD = int(os.getenv("BG_SUBTRACTOR_THRESHOLD", 16))
